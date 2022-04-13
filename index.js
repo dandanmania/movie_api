213b6b8e15b9b793e2757d2e1f3f1ec794c170db
dandanmania <danielkimus@gmail.com>
2022-04-13 14:03:26 -0700

    Add directors and genres to topMovies

--- index.js
+++ index.js
@@ -7,52 +7,72 @@ let topMovies = [
     {
         title: 'Kiki\'s Delivery Service',
         year: 1989,
-        studio: 'Studio Ghibli'
+        studio: 'Studio Ghibli',
+        director: ['Hayao Miyazaki'],
+        genre: ['Adventure', 'Comedy', 'Drama', 'Fantasy']
     },
     {
         title: 'Digimon: Our War Game!',
         year: 2000,
-        studio: 'Toei Animation'
+        studio: 'Toei Animation',
+        director: ['Mamoru Hosoda'],
+        genre: ['Adventure', 'Comedy', 'Sci-Fi']
     },
     {
         title: 'Card Captor Sakura Movie 2: The Sealed Card',
         year: 2000,
-        studio: 'Madhouse'
+        studio: 'Madhouse',
+        director: ['Morio Asaka'],
+        genre: ['Comedy', 'Drama', 'Fantasy', 'Romance']
     },
     {
         title: 'Detective Conan Movie 06: The Phantom of Baker Street',
         year: 2002,
-        studio: 'TMS Entertainment'
+        studio: 'TMS Entertainment',
+        director: ['Kenji Kodama'],
+        genre: ['Adventure', 'Mystery']
     },
     {
         title: 'The Girl Who Leapt Through Time',
         year: 2006,
-        studio: 'Madhouse'
+        studio: 'Madhouse',
+        director: ['Mamoru Hosoda'],
+        genre: ['Drama', 'Romance', 'Sci-Fi']
     },
     {
         title: 'The Disappearance of Haruhi Suzumiya',
         year: 2010,
-        studio: 'Kyoto Animation'
+        studio: 'Kyoto Animation',
+        director: ['Tatsuya Ishihara', 'Yasuhiro Takemoto'],
+        genre: ['Mystery', 'Romance', 'Sci-Fi', 'Supernatural']
     },
     {
         title: 'PERSONA 3 THE MOVIE - #4 Winter of Rebirth',
         year: 2016,
-        studio: 'A-1 Pictures'
+        studio: 'A-1 Pictures',
+        director: ['Tomohisa Taguchi'],
+        genre: ['Action', 'Fantasy', 'Supernatural']
     },
     {
         title: 'Digimon Adventure tri. 6: Bokura no Mirai',
         year: 2018,
-        studio: 'Toei Animation'
+        studio: 'Toei Animation',
+        director: ['Keitarou Motonaga'],
+        genre: ['Action', 'Adventure', 'Comedy', 'Drama']
     },
     {
         title: 'Fate/stay night Movie: Heaven\'s Feel - III. Spring Song',
         year: 2020,
-        studio: 'ufotable'
+        studio: 'ufotable',
+        director: ['Tomonori Sudou'],
+        genre: ['Action', 'Fantasy', 'Supernatural']
     },
     {
         title: 'Belle',
         year: 2021,
-        studio: 'Studio Chizu'
+        studio: 'Studio Chizu',
+        director: ['Mamoru Hosoda'],
+        genre: ['Drama', 'Fantasy']
     }
 ];
 
