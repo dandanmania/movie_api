60b142ea71d419444f18929b15002ba558ca36fe
dandanmania <danielkimus@gmail.com>
2022-04-12 18:18:41 -0700

    Update topMovies with studio data

--- index.js
+++ index.js
@@ -6,43 +6,53 @@ const app = express();
 let topMovies = [
     {
         title: 'Kiki\'s Delivery Service',
-        year: 1989
+        year: 1989,
+        studio: 'Studio Ghibli'
     },
     {
         title: 'Digimon: Our War Game!',
-        year: 2000
+        year: 2000,
+        studio: 'Toei Animation'
     },
     {
         title: 'Card Captor Sakura Movie 2: The Sealed Card',
-        year: 2000
+        year: 2000,
+        studio: 'Madhouse'
     },
     {
         title: 'Detective Conan Movie 06: The Phantom of Baker Street',
-        year: 2002
+        year: 2002,
+        studio: 'TMS Entertainment'
     },
     {
         title: 'The Girl Who Leapt Through Time',
-        year: 2006
+        year: 2006,
+        studio: 'Madhouse'
     },
     {
         title: 'The Disappearance of Haruhi Suzumiya',
-        year: 2010  
+        year: 2010,
+        studio: 'Kyoto Animation'
     },
     {
         title: 'PERSONA 3 THE MOVIE - #4 Winter of Rebirth',
-        year: 2016
+        year: 2016,
+        studio: 'A-1 Pictures'
     },
     {
         title: 'Digimon Adventure tri. 6: Bokura no Mirai',
-        year: 2018
+        year: 2018,
+        studio: 'Toei Animation'
     },
     {
         title: 'Fate/stay night Movie: Heaven\'s Feel - III. Spring Song',
-        year: 2020
+        year: 2020,
+        studio: 'ufotable'
     },
     {
         title: 'Belle',
-        year: 2021
+        year: 2021,
+        studio: 'Studio Chizu'
     }
 ];
 
