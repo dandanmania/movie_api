a3e22171003870f0be7a0ad486c6bd1d4d0ca570
dandanmania <danielkimus@gmail.com>
2022-07-01 19:10:13 -0700

    Update cors

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //CORS
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://dandan-myflix.netlify.app'];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of alloweed origins
+            let message = `The CORS policy for this application doesn't allow access from origin ` + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
